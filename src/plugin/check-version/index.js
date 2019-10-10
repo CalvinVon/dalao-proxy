ad88467007a3a765b8a3d482558bcfe553ebeeca
Calvin <vip935721016@163.com>
2019-10-10 19:39:01 +0800

    fix(plugin-check-version): fix checking and add changelog info

--- src/plugin/check-version/index.js
+++ src/plugin/check-version/index.js
@@ -1,23 +1,19 @@
 require('colors')
 const { spawn } = require('child_process');
 const { version } = require('../../../config')
-let versions;
 
 const REG_VERSION = /^(\d+)\.(\d+)\.(\d+)/;
 
 function checkVersion() {
-    const versionCmd = spawn('npm', ['show', 'dalao-proxy', 'time', '--json'], {
+    const versionCmd = spawn('npm', ['view', 'dalao-proxy', 'version', 'time', '--json'], {
         stdio: 'pipe',
         shell: true,
         env: process.env
     });
 
     versionCmd.stdout.on('data', data => {
-        versions = JSON.parse(data);
-        
-        const versionList = Object.keys(versions).sort().filter(version => REG_VERSION.test(version));
-        const latestVersion = versionList[versionList.length - 1];
-        
+        const { version: latestVersion, time } = JSON.parse(data);
+
         const _latest_version = latestVersion.match(REG_VERSION) || [];
         const _current_version = version.match(REG_VERSION) || [];
         const latest_MajorVer = _latest_version[1];
@@ -39,8 +35,11 @@ function checkVersion() {
         }
 
         if (whatUpdate) {
-            console.log(`\n\n> 🎉  A new ${whatUpdate} version (${latestVersion}) of dalao-proxy has published! Type \`npm i -g dalao-proxy@${latestVersion}\` to update.`.yellow)
-            console.log(`  See https://github.com/CalvinVon/dalao-proxy to get latest infomation of version ${latestVersion} \n\n`.grey);
+            const updateTime = time[latestVersion].replace(/T.+$/, '');
+            console.log(`
+> 🎉  A new ${whatUpdate} version (${latestVersion}) of dalao-proxy has published at ${updateTime}!
+  Type \`npm i -g dalao-proxy@${latestVersion}\` to update.`.yellow)
+            console.log(`  See https://github.com/CalvinVon/dalao-proxy/blob/master/CHANGELOG.md#${latest_MajorVer}${latest_MinorVer}${latest_FixVer}-${updateTime} to get latest infomation of version ${latestVersion} \n\n`.grey);
         }
         versionCmd.kill();
     });
