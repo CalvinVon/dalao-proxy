0608a8b17ec45c026d2533e6a521fb26088590b7
CalvinVon <vip935721016@163.com>
2024-08-01 02:16:55 +0800

    feat(hijack): use new child plugin API

--- packages/@calvin_von/plugin-request-hijack/commander.js
+++ packages/@calvin_von/plugin-request-hijack/commander.js
@@ -2,53 +2,46 @@ const path = require("path");
 const { version } = require("./package.json");
 
 RegExp.prototype.toJSON = function () {
-  return this.toString();
+    return this.toString();
 };
 
 /**
  * @param {import('./type').HijackOptions} pluginConfig
  */
 module.exports = function (program, register, pluginConfig) {
-  if (pluginConfig.enable) {
-    register.configure("config", (config, callback) => {
-      config.plugins.push([
-        "@calvin_von/proxy-plugin-inject",
-        {
-          defaultEnable: true,
-          optionsField: "hijack__inject",
-        },
-      ]);
-
-      config["hijack__inject"] = {
-        rules: [
-          {
-            test: config.requestHijack.page,
-            serves: {
-              "request-hijack": path.join(
-                __dirname,
-                "libs",
-                "request-hijack.js"
-              ),
-            },
-            template: `
-              <script>window.__hijackConfig = ${JSON.stringify(
-                {
-                  version,
-                  host: config.host,
-                  proxyTable: config.proxyTable,
-                  hijack: config.requestHijack,
-                },
-                null,
-                2
-              )}</script>
-              <script src="{{request-hijack}}"></script>
-            `,
-            insert: "head",
-          },
-        ],
-      };
-
-      callback(null, config);
-    });
-  }
+    if (pluginConfig.enable) {
+        register.setChildPlugin(
+            /** child plugin name */
+            "@calvin_von/proxy-plugin-inject",
+            /** child plugin field */
+            "hijack__inject",
+            /** child plugin config */
+            config => ({
+                rules: [
+                    {
+                        test: config.requestHijack.page,
+                        serves: {
+                            "request-hijack": path.join(
+                                __dirname,
+                                "libs",
+                                "request-hijack.js"
+                            ),
+                        },
+                        template: `\n<script>window.__hijackConfig = ${JSON.stringify(
+                            {
+                                version,
+                                host: config.host,
+                                proxyTable: config.proxyTable,
+                                hijack: config.requestHijack,
+                            },
+                            null,
+                            2
+                        )}</script>
+                            <script src="{{request-hijack}}"></script>\n`,
+                        insert: "head",
+                    },
+                ],
+            })
+        );
+    }
 };
